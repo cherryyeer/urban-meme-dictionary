5cd5a34bda508b47926b08a77df136340b5aa385
Hollusam <oluyomisamuels@gmail.com>
2023-01-11 20:54:12 +0000

    HTML, CSS and Javascript files updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,50 @@
 console.log("hello world");
 
+// Gets previous searches from local storage
+var storedHistory = JSON.parse(localStorage.getItem("storedHistory")) || [];
+
+if (storedHistory.length > 0) {
+  wordDefinition(storedHistory[storedHistory.length - 1]);
+}
+
+for (var i = 0; i < storedHistory.length; i++) {
+    
+    // Appends the row to history div
+    wordHistory(storedHistory[i]);
+}
+
+// Creating header
+var h4El = $("<h4>").text("Recent Searched Words");
+    h4El.css("display", "none");
+    h4El.addClass("header-history");
+    $("#history").append(h4El);
+
+function wordHistory(btn) {
+    
+
+    var wordBtn = $("<button>");
+    wordBtn.addClass("btn btn-sm history-btn");
+    wordBtn.text(btn);
+    $("#history").append(wordBtn);
+}
+
+$("#history").on("click", ".btn", function () {
+
+    $("#meme-display").empty();
+    $("#words-display").empty();
+    $("#searched-word").empty();
+
+    $("#memes").hide();
+    $("#words").hide();
+    $("#about").hide();
+    $(".results-gifs").show();
+    $('html,body').animate({scrollTop: $("#result").offset().top}, 500);
+
+    wordDefinition($(this).text());
+    renderGif($(this).text());
+  });
+
+
 function renderGif(wordSearch) {
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=6YBZyDMzPgGymZCqE5zHPD0fYhwaMlgK&q=" + wordSearch + "&limit=1&offset=0&rating=g&lang=en";
     $.ajax({
@@ -8,6 +53,18 @@ function renderGif(wordSearch) {
 }).then(function (response) {
     console.log(response);
     console.log(queryURL);
+
+    if (storedHistory.indexOf(wordSearch) === -1) {
+        // Pushing the search word into history array
+        storedHistory.push(wordSearch);
+  
+        // Storing the searched word
+        localStorage.setItem("storedHistory", JSON.stringify(storedHistory));
+        wordHistory(wordSearch);
+  
+      }
+      
+      $("#searched-word").empty();
     
     var giffy = response.data;
 
@@ -19,7 +76,7 @@ function renderGif(wordSearch) {
     };
 
 });
-
+fetch(queryURL);
 };
 
  //   URBAN Dictionary API
@@ -52,7 +109,6 @@ $("#search-button").on("click", function (event) {
     $("#words-display").empty();
     $("#searched-word").empty();
 
-    
     // Getting the value in
     var wordSearch = $("#search-query").val().trim();
   
@@ -63,8 +119,9 @@ $("#search-button").on("click", function (event) {
     $("#words").hide();
     $("#about").hide();
     $(".results-gifs").show();
+    $(".header-history").show();
     $('html,body').animate({scrollTop: $("#result").offset().top}, 500);
     
   renderGif(wordSearch);
-  wordDefinition(wordSearch)
+  wordDefinition(wordSearch);
   });
